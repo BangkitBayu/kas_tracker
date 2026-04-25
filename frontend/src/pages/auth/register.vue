ae44c30da9d128ee8605a57a0a9b6f95e0f49101
BangkitBayu <martinusmartinussaja@gmail.com>
2026-04-25 10:57:20 +0700

    first commit

---
+++ frontend/src/pages/auth/register.vue
@@ -0,0 +1,10 @@
+<script setup lang="js">
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/utils/axiosConfig.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+// Axios instance digunakan untuk mendefinisikan configurasi dasar axios
+const api = axios.create({
+  baseURL: "http://localhost:8000/api/v1/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+// (Interceptor/pencegat) request berjalan sebelum request dikirim ke api
+api.interceptors.request.use((config) => {
+  /**
+   * Interceptor auth digunakan untuk set authorization bearer ke client untuk mengakses route api dengan token
+   */
+
+  // Ambil token dari localstorage
+  const token = localStorage.getItem("token");
+
+  // Cek apakah token ada
+  if (token) {
+    console.log(token);
+    config.headers.Authorization = `Bearer ${token}`;
+    console.log(config.headers)
+  } else {
+    console.log("No token available");
+  }
+});
+
+
+export default api;
