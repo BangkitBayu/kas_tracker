0756f63e015326d244c2be3011b40907f7fbd4a1
BangkitBayu <martinusmartinussaja@gmail.com>
2026-05-03 20:47:37 +0700

    setup migration

---
+++ backend/database/migrations/2026_05_03_131659_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('member_name');
+            $table->integer('nominal');
+            $table->date('date');
+            $table->timestamps();
+
+            $table->foreign('cash_id')->references('id')->on('cash');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
